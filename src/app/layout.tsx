a560a5ffe7fe152866cebfc00b51e5680e0534bd
Hombre2021 <hombre2021@gmail.com>
2026-06-12 11:41:08 -0600

    perf: defer GTM/GA boot to lazyOnload for lower mobile main-thread impact

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -127,7 +127,7 @@ export default function RootLayout({
           </noscript>
         ) : null}
         {gtmId ? (
-          <Script id="gtm-init" strategy="afterInteractive">
+          <Script id="gtm-init" strategy="lazyOnload">
             {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');`}
           </Script>
         ) : null}
@@ -135,10 +135,10 @@ export default function RootLayout({
           <>
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
-              strategy="afterInteractive"
+              strategy="lazyOnload"
               async
             />
-            <Script id="ga4-init" strategy="afterInteractive">
+            <Script id="ga4-init" strategy="lazyOnload">
               {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} window.gtag = gtag; gtag('js', new Date()); gtag('config', '${gaMeasurementId}', { send_page_view: ${gtmId ? "false" : "true"} });`}
             </Script>
           </>
